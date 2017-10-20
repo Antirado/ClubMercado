c18cd8a237701ca849b6c628d36f2803afbd93aa
Antirado <andrestirado15@gmail.com>
2017-10-20 02:41:24 -0400

    Finished screen 4

--- app/src/main/java/com/analiticom/clubmercado/ComprasDelDia.java
+++ app/src/main/java/com/analiticom/clubmercado/ComprasDelDia.java
@@ -12,12 +12,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link landing.OnFragmentInteractionListener} interface
+ * {@link ComprasDelDia.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link landing#newInstance} factory method to
+ * Use the {@link ComprasDelDia#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class landing extends Fragment {
+public class ComprasDelDia extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -29,7 +29,7 @@ public class landing extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public landing() {
+    public ComprasDelDia() {
         // Required empty public constructor
     }
 
@@ -39,11 +39,11 @@ public class landing extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment landing.
+     * @return A new instance of fragment ComprasDelDia.
      */
     // TODO: Rename and change types and number of parameters
-    public static landing newInstance(String param1, String param2) {
-        landing fragment = new landing();
+    public static ComprasDelDia newInstance(String param1, String param2) {
+        ComprasDelDia fragment = new ComprasDelDia();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -64,7 +64,7 @@ public class landing extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_landing, container, false);
+        return inflater.inflate(R.layout.fragment_compras_del_dia, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
